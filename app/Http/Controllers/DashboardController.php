05cbdce4f5f4b06ba2ee603e3a92c79042099943
mas-yan <mrey5597@gmail.com>
2022-11-07 22:37:01 +0700

    fix: count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,8 +16,28 @@ class DashboardController extends Controller
     public function __invoke()
     {
         $pc = Client::count();
-        $terhubung = Client::where('status', 'terhubung')->count();
-        $tidakTerhubung = Client::where('status', 'tidak terhubung')->count();
-        return view('page.dashboard', compact('pc', 'terhubung', 'tidakTerhubung'));
+        $client = Client::get();
+        $terhubung = [];
+        $tidakTerhubung = [];
+        foreach ($client as $value) {
+            $host = $value->ip;
+            $ch = curl_init($host);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $data = curl_exec($ch);
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+            if ($httpcode !== 0) {
+                $terhubung[] = $value->ip;
+            } else {
+                $tidakTerhubung[] = $value->ip;
+            }
+        }
+        return view('page.dashboard', [
+            'pc' => $pc,
+            'terhubung' => count($terhubung),
+            'tidakTerhubung' => count($tidakTerhubung),
+        ]);
     }
 }
